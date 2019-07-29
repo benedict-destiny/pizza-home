b6b72bbed6c84cbcd156ef3c3b70c5f87e70a229
47 <bendestiny1289@gmail.com>
2019-07-29 12:16:17 +0300

    Add selcet size form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+const large = parseInt(1000);
+ function l(){
+}
+const medium = parseInt(800);
+ function mid(){
+
+ }
+const small = parseInt();
+function smal(){
+
+}
 $(document).ready(function(){
     $(".order").click(function(){
         $("#note").fadeOut();
@@ -13,32 +24,99 @@ $(document).ready(function(){
         $(".cancel").fadeOut();
         $(".size1").fadeOut();
         $(".checkout").fadeOut();
-;    })
+        })
+    $(".cancel2").click(function(){
+        $(".note2").fadeIn();
+        $("#para2").slideToggle();
+        $(".order2").slideToggle();
+        $(".cancel2").fadeOut();
+        $(".size2").fadeOut();
+    })
+    $(".order2").click(function(){
+        $(".note2").fadeOut();
+        $("#para2").fadeIn();
+        $(".order2").hide();
+        $(".cancel2").fadeIn();
+        $(".size2").fadeIn();
+    })
 })
-function selectSize() {
+
+
+function selectLarge() {
     var size = document.getElementById("size").value;
     if (size === "1") {
-      alert("OOps seems like you havent selected a size");
+      alert("OOps seems like you haven't selected a size");
       return false;
     }
-    else if (size==="2"){
-        $(".checkout").fadeIn();
-        $("#section2").fadeOut();
-        $("#click2").fadeIn();
-        $("#click").hide();
-        $(".out").show();
-    }
-    else if (size==="3"){
-        $(".checkout").fadeIn();
-        $("#section2").fadeOut();
-        $("#click2").fadeIn();
-        $("#click").hide();
-    }
-    else if (size==="4"){
-        $(".checkout").fadeIn();
-        $("#section2").fadeOut();
-        $("#click2").fadeIn();
-        $("#click").hide();
-    }
+    // else if (size==="2"){
+    //     $(".checkout").fadeIn();
+    //     $("#section2").fadeOut();
+    //     $("#click2").fadeIn();
+    //     $("#click").hide();
+    //     $(".out").show();
+    // }
+    // else if (size==="3"){
+    //     $(".checkout").fadeIn();
+    //     $("#section2").fadeOut();
+    //     $("#click2").fadeIn();
+    //     $("#click").hide();
+    //     $(".out").show();
+    // }
+    // else if (size==="4"){
+    //     $(".checkout").fadeIn();
+    //     $("#section2").fadeOut();
+    //     $("#click2").fadeIn();
+    //     $("#click").hide();
+    // }
     
   }
+function select() {
+    var size = document.getElementById("size").value;
+    if (size === "1") {
+      alert("OOps seems like you haven't selected a size");
+      return false;
+    }
+    // else if (size==="2"){
+    //     $(".checkout").fadeIn();
+    //     $("#section2").fadeOut();
+    //     $("#click2").fadeIn();
+    //     $("#click").hide();
+    //     $(".out").show();
+    //     document.getElementById("total").innerHTML = "the total is: " + large;
+    // }
+    // else if (size==="3"){
+    //     $(".checkout").fadeIn();
+    //     $("#section2").fadeOut();
+    //     $("#click2").fadeIn();
+    //     $("#click").hide();
+    //     $(".out").show();
+    // }
+    // else if (size==="4"){
+    //     $(".checkout").fadeIn();
+    //     $("#section2").fadeOut();
+    //     $("#click2").fadeIn();
+    //     $("#click").hide();
+    // }
+    
+  }
+function selectQuantity(){
+ var many=parseInt($("#num").val())
+    if (many< 1 ){
+    alert("you havent specified on the number of pizaas")
+    return false;
+    }
+    else if (many>1 || many==1) {
+    $(".checkout").fadeIn();
+    $("#section2").fadeOut();
+    $("#click2").fadeIn();
+    $("#click").hide();
+    $(".out").show(); 
+    var tot = large * many
+    document.getElementById("total").innerHTML= "total is: " + tot;
+    }
+
+    
+}
+l();
+mid();
+smal();

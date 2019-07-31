eab230b82996128747c66a44a7f60ba3dd2c1d72
47 <bendestiny1289@gmail.com>
2019-07-31 07:11:31 +0300

    add an order delivery option

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+var sze = parseInt(document.getElementById("size").value)
+ function the(){
+
+ }
 const large = parseInt(1000);
  function l(){
 }
@@ -18,54 +22,54 @@ $(document).ready(function(){
         $(".size1").fadeIn(800);
     })
     $(".cancel").click(function(){
-        $("#note").fadeIn(500);
+        $("#note").fadeIn(800);
         $("#para").slideToggle(500);
         $(".order").slideToggle(500);
-        $(".cancel").fadeOut(00);
-        $(".size1").fadeOut(500);
-        $(".checkout").fadeOut(500);
+        $(".cancel").fadeOut(0);
+        $(".size1").fadeOut(0);
+        $(".checkout").fadeOut(0);
         })
     $(".cancel2").click(function(){
         $(".note2").fadeIn(500);
         $("#para2").slideToggle(500);
         $(".order2").slideToggle(500);
-        $(".cancel2").fadeOut(500);
-        $(".size2").fadeOut(500);
+        $(".cancel2").fadeOut(0);
+        $(".size2").fadeOut(0);
     })
     $(".order2").click(function(){
-        $(".note2").fadeOut(500);
+        $(".note2").fadeOut(00);
         $("#para2").fadeIn(500);
         $(".order2").hide();
-        $(".cancel2").fadeIn(500);
-        $(".size2").fadeIn(500);
+        $(".cancel2").fadeIn(800);
+        $(".size2").fadeIn(800);
     })
     $(".cancel3").click(function(){
         $(".note3").fadeIn(500);
         $("#para3").slideToggle(500);
         $(".order3").slideToggle(500);
-        $(".cancel3").fadeOut(500);
-        $(".size3").fadeOut(500);
+        $(".cancel3").fadeOut(0);
+        $(".size3").fadeOut(0);
     })
     $(".order3").click(function(){
-        $(".note3").fadeOut(500);
+        $(".note3").fadeOut(0);
         $("#para3").fadeIn(500);
         $(".order3").hide();
-        $(".cancel3").fadeIn(500);
-        $(".size3").fadeIn(500);
+        $(".cancel3").fadeIn(800);
+        $(".size3").fadeIn(800);
     })
     $(".cancel4").click(function(){
         $(".note4").fadeIn(500);
         $("#para4").slideToggle(500);
         $(".order4").slideToggle(500);
-        $(".cancel4").fadeOut(500);
-        $(".size4").fadeOut(500);
+        $(".cancel4").fadeOut(0);
+        $(".size4").fadeOut(0);
     })
     $(".order4").click(function(){
-        $(".note4").fadeOut(500);
+        $(".note4").fadeOut(0);
         $("#para4").fadeIn(500);
         $(".order4").hide();
-        $(".cancel4").fadeIn(500);
-        $(".size4").fadeIn(500);
+        $(".cancel4").fadeIn(800);
+        $(".size4").fadeIn(800);
     })
 })
 
@@ -99,11 +103,13 @@ function selectLarge() {
     
   }
 function selectLarge() {
-    var size = document.getElementById("size").value;
-    if (size < "1") {
-      alert("OOps seems like you haven't selected a size");
-      return false;
-    }
+   
+        alert(sze)
+    // var size = document.getElementById("size").value;
+    // if (size < "1") {
+    //   alert("OOps seems like you haven't selected a size");
+    //   return false;
+    // }
     // else if (size==="2"){
     //     $(".checkout").fadeIn();
     //     $("#section2").fadeOut();
@@ -142,13 +148,25 @@ function selectQuantity(){
     var tot = large * many
     document.getElementById("total").innerHTML= "total is: " + tot;
     }
+ 
+}
 
-    
-}
 function post(){
-    alert("your order has been received thank you for flambeing")
-    return true
+
+    var response=confirm("wold you like your order to be delivered to you location")
+    if (response == true){
+        var loc = prompt("enter your location");
+        var main =loc.toUpperCase()
+        alert("order has been received and will be delivered to " + main);  
+    }
+    else if(response==false){
+    alert("your order has been received You'll receive a message once your order is ready. Thank you for flambeing");
+    return true;
+    }
 }
+
+
 l();
 mid();
 smal();
+the()
